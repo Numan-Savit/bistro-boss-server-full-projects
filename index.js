19a24d33af0b8496a5c94f86a3e6106bca5a8393
Abdullah Al Numan <abdullahalnuman7610@gmail.com>
2025-04-09 09:58:14 +0600

    user create and login logout page connection mongodb

---
+++ index.js
@@ -0,0 +1,75 @@
+// step-15________________________________________________________________________________________3
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ymvr3sa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // step-16_________________________________________________________________________________1
+
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewsCollection = client.db("bistroDb").collection("reviews");  //step-17_________________2
+
+    app.get('/menu', async(req, res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+
+    // step-16__________________________________________________________________________________1
+
+    // step-17___________________________________________________________________________________1
+
+    app.get('/reviews', async(req, res)=>{
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+    // step-17___________________________________________________________________________________2
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Bistro Boss Server!')
+})
+
+app.listen(port, () => {
+    console.log(`Bistro Boss Server is running on port: ${port}`)
+})
